a9f043c43b2770971f57d47ba9708c4b71fd5d1e
Surojiddin <isurojiddin@gmail.com>
2022-05-28 16:56:14 +0500

    SP-7 & SP-9

--- modules/tests/src/main/scala/smsplatform/stub_services/SMSTemplatesStub.scala
+++ modules/tests/src/main/scala/smsplatform/stub_services/SMSTemplatesStub.scala
@@ -2,9 +2,12 @@ package smsplatform.stub_services
 
 import com.itforelead.smspaltfrom.domain.SMSTemplate
 import com.itforelead.smspaltfrom.domain.SMSTemplate.CreateSMSTemplate
+import com.itforelead.smspaltfrom.domain.types.TemplateId
 import com.itforelead.smspaltfrom.services.SMSTemplates
 
 class SMSTemplatesStub[F[_]] extends SMSTemplates[F] {
   override def create(param: CreateSMSTemplate): F[SMSTemplate] = ???
   override def templates: F[List[SMSTemplate]] = ???
+  override def update(param: SMSTemplate): F[SMSTemplate] = ???
+  override def delete(id: TemplateId): F[Unit] = ???
 }
