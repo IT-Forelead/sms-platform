d105187995ab1592e6e3c31105bd11a6c50a4a2d
Prince <prince777_98@mail.ru>
2022-06-02 10:03:12 +0500

    Integration test changes

---
+++ modules/tests/src/it/resources/tables.sql
@@ -0,0 +1,41 @@
+CREATE TYPE GENDER AS ENUM ('male', 'female');
+CREATE TYPE ROLE AS ENUM ('admin', 'user');
+CREATE TYPE STATUS AS ENUM ('sent', 'delivered', 'failed', 'unknown');
+
+CREATE TABLE IF NOT EXISTS users (
+  uuid UUID PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE NOT NULL,
+  gender GENDER NOT NULL,
+  password VARCHAR NOT NULL,
+  role ROLE NOT NULL DEFAULT 'user'
+);
+
+INSERT INTO "users" ("uuid", "name", "email", "gender", "password", "role")
+VALUES ('c1039d34-425b-4f78-9a7f-893f5b4df478', 'Admin', 'saroyadmin@gmail.com', 'male',
+        '$s0$e0801$5JK3Ogs35C2h5htbXQoeEQ==$N7HgNieSnOajn1FuEB7l4PhC6puBSq+e1E8WUaSJcGY=', 'admin');
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    birthday DATE NOT NULL,
+    phone VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sms_templates (
+    id UUID PRIMARY KEY,
+    text VARCHAR NOT NULL,
+    active BOOLEAN NOT NULL,
+    deleted BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    contact_id UUID NOT NULL CONSTRAINT fk_contact_id REFERENCES contacts (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    sms_temp_id UUID NOT NULL CONSTRAINT fk_sms_temp_id REFERENCES sms_templates (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    sent_date TIMESTAMP NOT NULL,
+    delivery_status STATUS NOT NULL
+);
---
+++ modules/tests/src/it/scala/smsplatform/services/UsersSuite.scala
@@ -0,0 +1,23 @@
+package smsplatform.services
+
+import cats.effect.IO
+import com.itforelead.smspaltfrom.services.Users
+import eu.timepit.refined.auto.autoUnwrap
+import smsplatform.utils.DBSuite
+import smsplatform.utils.Generators.createUserGen
+import tsec.passwordhashers.jca.SCrypt
+
+object UsersSuite extends DBSuite {
+
+  test("Create User") { implicit postgres =>
+    val users = Users[IO]
+    forall(createUserGen) { createUser =>
+      SCrypt.hashpw[IO](createUser.password).flatMap { hash =>
+        for {
+          user1 <- users.create(createUser, hash)
+          user2 <- users.find(user1.email)
+        } yield assert(user2.exists(_.user == user1))
+      }
+    }
+  }
+}
