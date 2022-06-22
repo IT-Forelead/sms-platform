690ea059b0cd7f01a728a6abc0f89a6a4a583b7a
Prince <prince777_98@mail.ru>
2022-06-22 10:36:30 +0500

    send congratulation for Holidays

--- modules/tests/src/main/scala/smsplatform/stub_services/HolidaysStub.scala
+++ modules/tests/src/main/scala/smsplatform/stub_services/HolidaysStub.scala
@@ -6,9 +6,10 @@ import com.itforelead.smspaltfrom.domain.types.HolidayId
 import com.itforelead.smspaltfrom.services.Holidays
 
 class HolidaysStub[F[_]] extends Holidays[F] {
-  override def create(holidayParam: CreateHoliday): F[Holiday]                        = ???
-  override def holidays: F[List[Holiday]]                                             = ???
-  override def update(holiday: UpdateHoliday): F[Holiday]                             = ???
-  override def updateTemplateInHoliday(holiday: UpdateTemplateInHoliday): F[Holiday]  = ???
-  override def delete(id: HolidayId): F[Unit]                                         = ???
+  override def create(holidayParam: CreateHoliday): F[Holiday]                       = ???
+  override def holidays: F[List[Holiday]]                                            = ???
+  override def update(holiday: UpdateHoliday): F[Holiday]                            = ???
+  override def updateTemplateInHoliday(holiday: UpdateTemplateInHoliday): F[Holiday] = ???
+  override def delete(id: HolidayId): F[Unit]                                        = ???
+  override def holidaysOfToday: F[List[Holiday]]                                     = ???
 }
